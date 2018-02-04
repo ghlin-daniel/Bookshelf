0e470dc5c5bf2cf4e21b904f25f84bdba18e43a3
Daniel Lin <brickgit@gmail.com>
2018-02-03 19:18:59 -0800

    Add index page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+
+
+def index(request):
+    books = Book.objects.order_by('-added_date')[:30]
+    return render(request, 'books/index.html', {'books': books})
