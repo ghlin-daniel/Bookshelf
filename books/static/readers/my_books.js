44126f9e7327165648dcabf338f3c451204efe1b
Daniel Lin <brickgit@gmail.com>
2018-06-02 18:15:09 -0700

    Buttons are non-clickable when running async operations

--- books/static/readers/my_books.js
+++ books/static/readers/my_books.js
@@ -1,275 +1,298 @@
 function clearViews() {
-    $("#reading-book-title").empty();
-    $("#reading-progress").text("");
-    $("#reading-list").empty();
+  $("#reading-book-title").empty();
+  $("#reading-progress").text("");
+  $("#reading-list").empty();
+}
+
+function disableViews(disabled) {
+  $(".btn").prop("disabled", disabled);
+  $(".reading-date-from").prop("disabled", disabled);
+  $(".reading-date-to").prop("disabled", disabled);
 }
 
 function updateReadingList(readings) {
-    var actionFinish = $("#action-finish");
-    var actionRead = $("#action-read");
-    var actionAbandon = $("#action-abandon");
-    var actionRemoveBook = $("#action-remove-book");
+  var actionFinish = $("#action-finish");
+  var actionRead = $("#action-read");
+  var actionAbandon = $("#action-abandon");
+  var actionRemoveBook = $("#action-remove-book");
 
-    if (readings.length == 0) {
-        $("#reading-progress").text("Unread");
+  if (readings.length == 0) {
+    $("#reading-progress").text("Unread");
+    actionFinish.hide();
+    actionRead.show();
+    actionAbandon.hide();
+  }
+  else {
+    var latest = readings[0];
+    switch (latest.progress) {
+      case 'R':
+        $("#reading-progress").text("Reading");
+        actionFinish.show();
+        actionRead.hide();
+        actionAbandon.show();
+        break;
+
+      case 'F':
+        $("#reading-progress").text("Finished");
         actionFinish.hide();
         actionRead.show();
         actionAbandon.hide();
+        break;
+
+      case 'A':
+        $("#reading-progress").text("Abandoned");
+        actionFinish.hide();
+        actionRead.show();
+        actionAbandon.hide();
+        break;
     }
-    else {
-        var latest = readings[0];
-        switch (latest.progress) {
-            case 'R':
-                $("#reading-progress").text("Reading");
-                actionFinish.show();
-                actionRead.hide();
-                actionAbandon.show();
-                break;
+  }
 
-            case 'F':
-                $("#reading-progress").text("Finished");
-                actionFinish.hide();
-                actionRead.show();
-                actionAbandon.hide();
-                break;
+  var statuses = {
+    "R": "Reading",
+    "F": "Finished",
+    "A": "Abandoned"
+  };
 
-            case 'A':
-                $("#reading-progress").text("Abandoned");
-                actionFinish.hide();
-                actionRead.show();
-                actionAbandon.hide();
-                break;
-        }
-    }
+  for (var index in readings) {
+    var reading = readings[index];
+    var from = "<td><input class='form-control form-control-sm reading-date-from' type='text' readonly value='" + reading.start_date + "'></td>";
+    var isEnded = reading.end_date != null;
+    var to = "<td><input class='form-control form-control-sm reading-date-to' type='text' readonly " + (isEnded ? "" : "hidden ") + "value='" + (isEnded ? reading.end_date : "") + "'></td>";
+    var progress = "<td class='align-middle'>" + statuses[reading.progress] + "</td>";
+    var btnDelete = "<td><button class='btn btn-danger btn-sm delete-reading' href='#'>Delete</a></td>";
+    var btnSave = "<td><button class='btn btn-primary btn-sm save-reading' href='#'>Save</a></td>";
+    $("#reading-list").append("<tr reading-id='" + reading.id + "'>" + from + " " + to + progress + btnDelete + btnSave + "</tr>");
+  }
 
-    var statuses = {
-        "R": "Reading",
-        "F": "Finished",
-        "A": "Abandoned"
-    };
+  $("tr").each(function() {
+    checkDatePickers($(this));
+  });
 
-    for (var index in readings) {
-        var reading = readings[index];
-        var from = "<td><input class='form-control form-control-sm reading-date reading-date-from' type='text' readonly value='" + reading.start_date + "'></td>";
-        var isEnded = reading.end_date != null;
-        var to = "<td><input class='form-control form-control-sm reading-date reading-date-to' type='text' readonly " + (isEnded ? "" : "hidden ") + "value='" + (isEnded ? reading.end_date : "") + "'></td>";
-        var progress = "<td class='align-middle'>" + statuses[reading.progress] + "</td>";
-        var btnDelete = "<td><a class='btn btn-danger btn-sm delete-reading' href='#'>Delete</a></td>";
-        var btnSave = "<td><a class='btn btn-primary btn-sm save-reading' href='#'>Save</a></td>";
-        $("#reading-list").append("<tr reading-id='" + reading.id + "'>" + from + " " + to + progress + btnDelete + btnSave + "</tr>");
-    }
-
-    $("tr").each(function() {
-        checkDatePickers($(this));
-    });
-
-    $(".delete-reading").click(onDeleteClick);
-    $(".save-reading").hide();
-    $(".save-reading").click(onSaveClick);
+  $(".delete-reading").click(onDeleteClick);
+  $(".save-reading").hide();
+  $(".save-reading").click(onSaveClick);
 }
 
 function checkDatePickers(row) {
-    var inputDateFrom = row.find(".reading-date-from");
-    var inputDateTo = row.find(".reading-date-to");
-    var dateFrom = inputDateFrom.attr("value");
-    var dateTo = inputDateTo.attr("value");
+  var inputDateFrom = row.find(".reading-date-from");
+  var inputDateTo = row.find(".reading-date-to");
+  var dateFrom = inputDateFrom.attr("value");
+  var dateTo = inputDateTo.attr("value");
 
-    var onDateSelect = function(dateText, inst) {
-        var oldValue = $(this).attr("value");
-        if (oldValue == dateText) return;
+  var onDateSelect = function(dateText, inst) {
+    var oldValue = $(this).attr("value");
+    if (oldValue == dateText) return;
 
-        $(this).attr("value", dateText);
-        var readingRow = $(this).closest("tr");
-        var btnSave = readingRow.find(".save-reading");
-        btnSave.show("normal");
-        checkDatePickers(readingRow);
-    };
+    $(this).attr("value", dateText);
+    var readingRow = $(this).closest("tr");
+    var btnSave = readingRow.find(".save-reading");
+    btnSave.show("normal");
+    checkDatePickers(readingRow);
+  };
 
-    inputDateFrom.datepicker("destroy");
-    inputDateFrom.datepicker({
-        dateFormat: 'yy-mm-dd',
-        maxDate: dateTo,
-        onSelect: onDateSelect,
-    });
+  inputDateFrom.datepicker("destroy");
+  inputDateFrom.datepicker({
+    dateFormat: 'yy-mm-dd',
+    maxDate: dateTo,
+    onSelect: onDateSelect,
+  });
 
-    inputDateTo.datepicker("destroy");
-    inputDateTo.datepicker({
-        dateFormat: 'yy-mm-dd',
-        minDate: dateFrom,
-        onSelect: onDateSelect,
-    });
+  inputDateTo.datepicker("destroy");
+  inputDateTo.datepicker({
+    dateFormat: 'yy-mm-dd',
+    minDate: dateFrom,
+    onSelect: onDateSelect,
+  });
 }
 
 function onDeleteClick() {
-    if (!confirm("Are you sure to delete the reading?")) return;
+  if (!confirm("Are you sure to delete the reading?")) return;
 
-    var row = $(this).closest("tr");
-    var readingId = row.attr("reading-id");
+  disableViews(true);
 
-    var isbn13 = $("#reading-dialog").attr("book-isbn13");
+  var row = $(this).closest("tr");
+  var readingId = row.attr("reading-id");
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13 + "/delete/",
-        data: { "reading-id": readingId },
-        dataType: 'json',
-        type: 'POST',
-        beforeSend: function(xhr, settings) {
-            if (!this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
-            }
-        },
-        success: function(response) {
-            clearViews();
+  var isbn13 = $("#reading-dialog").attr("book-isbn13");
 
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13 + "/delete/",
+    data: { "reading-id": readingId },
+    dataType: 'json',
+    type: 'POST',
+    beforeSend: function(xhr, settings) {
+      if (!this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+      }
+    },
+    success: function(response) {
+      clearViews();
+
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onSaveClick() {
-    if (!confirm("Are you sure to update the reading?")) return;
+  if (!confirm("Are you sure to update the reading?")) return;
 
-    var row = $(this).closest("tr");
-    var readingId = row.attr("reading-id");
-    var inputDateFrom = row.find(".reading-date-from");
-    var inputDateTo = row.find(".reading-date-to");
-    var dateFrom = inputDateFrom.attr("value");
-    var dateTo = inputDateTo.attr("value");
+  disableViews(true);
 
-    var isbn13 = $("#reading-dialog").attr("book-isbn13");
+  var row = $(this).closest("tr");
+  var readingId = row.attr("reading-id");
+  var inputDateFrom = row.find(".reading-date-from");
+  var inputDateTo = row.find(".reading-date-to");
+  var dateFrom = inputDateFrom.attr("value");
+  var dateTo = inputDateTo.attr("value");
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13 + "/update/",
-        data: { "reading-id": readingId, "start-date": dateFrom, "end-date": dateTo },
-        dataType: 'json',
-        type: 'POST',
-        beforeSend: function(xhr, settings) {
-            if (!this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
-            }
-        },
-        success: function(response) {
-            clearViews();
+  var isbn13 = $("#reading-dialog").attr("book-isbn13");
 
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13 + "/update/",
+    data: { "reading-id": readingId, "start-date": dateFrom, "end-date": dateTo },
+    dataType: 'json',
+    type: 'POST',
+    beforeSend: function(xhr, settings) {
+      if (!this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+      }
+    },
+    success: function(response) {
+      clearViews();
+
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onReadingLinkClick() {
-    clearViews();
+  clearViews();
+  disableViews(true);
 
-    var isbn13 = $(this).attr("book-isbn13");
-    var removeUrl = $(this).attr("remove-url");
+  var isbn13 = $(this).attr("book-isbn13");
+  var removeUrl = $(this).attr("remove-url");
 
-    var readingDialog = $("#reading-dialog");
-    readingDialog.attr("book-isbn13", isbn13);
-    readingDialog.attr("remove-url", removeUrl);
+  var readingDialog = $("#reading-dialog");
+  readingDialog.attr("book-isbn13", isbn13);
+  readingDialog.attr("remove-url", removeUrl);
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13,
-        dataType: 'json',
-        success: function(response) {
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13,
+    dataType: 'json',
+    success: function(response) {
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onActionReadClick() {
-    if (!confirm("Are you sure to start reading this book?")) return;
+  if (!confirm("Are you sure to start reading this book?")) return;
 
-    var isbn13 = $("#reading-dialog").attr("book-isbn13");
+  disableViews(true);
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13 + '/read/',
-        dataType: 'json',
-        success: function(response) {
-            clearViews();
+  var isbn13 = $("#reading-dialog").attr("book-isbn13");
 
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13 + '/read/',
+    dataType: 'json',
+    success: function(response) {
+      clearViews();
+
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onActionFinishClick() {
-    if (!confirm("Are you sure to finish reading this book?")) return;
+  if (!confirm("Are you sure to finish reading this book?")) return;
 
-    var isbn13 = $("#reading-dialog").attr("book-isbn13");
+  disableViews(true);
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13 + '/finish/',
-        dataType: 'json',
-        success: function(response) {
-            clearViews();
+  var isbn13 = $("#reading-dialog").attr("book-isbn13");
 
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13 + '/finish/',
+    dataType: 'json',
+    success: function(response) {
+      clearViews();
+
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onActionAbandonClick() {
-    if (!confirm("Are you sure to give up reading this book?")) return;
+  if (!confirm("Are you sure to give up reading this book?")) return;
 
-    var isbn13 = $("#reading-dialog").attr("book-isbn13");
+  disableViews(true);
 
-    $.ajax({
-        url: '/bookshelf/' + isbn13 + '/abandon/',
-        dataType: 'json',
-        success: function(response) {
-            clearViews();
+  var isbn13 = $("#reading-dialog").attr("book-isbn13");
 
-            if (response.book) {
-                var book = response.book;
-                $("#reading-book-title").text(book.title);
-            }
-            if (response.readings) {
-                updateReadingList(response.readings);
-            }
-        }
-    });
+  $.ajax({
+    url: '/bookshelf/' + isbn13 + '/abandon/',
+    dataType: 'json',
+    success: function(response) {
+      clearViews();
+
+      if (response.book) {
+        var book = response.book;
+        $("#reading-book-title").text(book.title);
+      }
+      if (response.readings) {
+        updateReadingList(response.readings);
+      }
+      disableViews(false);
+    }
+  });
 }
 
 function onActionRemoveBookClick() {
-    if (!confirm("Are you sure to remove this book from your bookshelf?")) return;
+  if (!confirm("Are you sure to remove this book from your bookshelf?")) return;
 
-    var removeUrl = $("#reading-dialog").attr("remove-url");
-    location.href = removeUrl
+  var removeUrl = $("#reading-dialog").attr("remove-url");
+  location.href = removeUrl
 }
 
 $(document).ready(function() {
-    $(".reading-link").click(onReadingLinkClick);
-    $("#action-read").click(onActionReadClick);
-    $("#action-finish").click(onActionFinishClick);
-    $("#action-abandon").click(onActionAbandonClick);
-    $("#action-remove-book").click(onActionRemoveBookClick);
+  $(".reading-link").click(onReadingLinkClick);
+  $("#action-read").click(onActionReadClick);
+  $("#action-finish").click(onActionFinishClick);
+  $("#action-abandon").click(onActionAbandonClick);
+  $("#action-remove-book").click(onActionRemoveBookClick);
 });
